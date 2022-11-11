1576b99e3ce668e4f8dc7ee3599e7fb44e4caf87
Alex Galax <alexgalax@protonmail.com>
2022-11-11 22:30:12 +0100

    node server fix

--- server-prod.js
+++ server-prod.js
@@ -1,23 +1,14 @@
 const dotenv = require('dotenv');
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 
 dotenv.config();
 
 const app = express();
 
-const credentials = {
-    key: fs.readFileSync(__dirname + '/sslcert/private.key'),
-    cert: fs.readFileSync(__dirname + '/sslcert/server.crt')
-};
+app.listen(process.env.PORT, ()=>{
+   console.log('server is running at port ' + process.env.PORT)
+});
 
-https
-    .createServer(credentials, app)
-    .listen(process.env.PORT, ()=>{
-        console.log('server is running at port ' + process.env.PORT)
-    });
-
-app.get('/', (req,res)=>{
-    res.send("Hello from your express server.")
+app.get('/', (req,response)=>{
+    response.sendFile((__dirname) + '/dist/index.html');
 })
