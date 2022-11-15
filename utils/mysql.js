002da2298b6ef1c46fc5d95bf329276cdb76de29
Alex Galax <alexgalax@protonmail.com>
2022-11-15 20:48:00 +0100

    fix

--- utils/mysql.js
+++ utils/mysql.js
@@ -6,7 +6,8 @@ exports.mysqlAddConversation = function(userId, prompt, completion, state, compl
     const connection = mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     });
 
     connection.connect(function(err) {
